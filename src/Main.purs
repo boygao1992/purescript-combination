dad641cfd1c6fcfc008d1b8c641eaa12ca817f87
boygao1992 <wg2313@columbia.edu>
2021-09-15 01:10:21 -0400

    1.4

--- src/Main.purs
+++ src/Main.purs
@@ -11,8 +11,20 @@ import Effect.Class.Console as Effect.Class.Console
 
 main :: Effect Unit
 main = do
-  _1_2
-  _1_3
+  pure unit
+
+_1_1 :: Effect Unit
+_1_1 = do
+  let
+    deck :: Int
+    deck = 40
+
+    hand :: Int
+    hand = 5
+  Effect.Class.Console.log
+    $ print1DTable "n" "prob"
+    $ flip map (Data.Array.range 1 10) \n ->
+        _1_1_equation deck hand n <> "%"
 
 _1_2 :: Effect Unit
 _1_2 = do
@@ -23,47 +35,96 @@ _1_2 = do
     hand :: Int
     hand = 5
   Effect.Class.Console.log
-    $ printTable "n \\ m"
+    $ print2DTable "n \\ m"
     $ flip map (Data.Array.range 1 10) \n ->
         flip map (Data.Array.range 1 10) \m ->
           _1_2_equation deck hand n m <> "%"
 
-_1_3 :: Effect Unit
-_1_3 = do
+_1_3 :: Int -> Effect Unit
+_1_3 v = do
   let
-    v :: Int
-    v = 6
-
     deck :: Int
     deck = 40
 
     hand :: Int
     hand = 5
   Effect.Class.Console.log
-    $ printTable "n \\ u"
-    $ flip map (Data.Array.range 1 10) \n ->
-        flip map (Data.Array.range 1 3) \u ->
-          _1_3_equation deck hand n v u <> "%"
+    ( [ printHeader
+          ( [ [ "n \\ u" ]
+            , Data.Array.range 0 3 <#> show
+            ]
+              # Data.Array.fold
+          )
+      , printBody
+          $ flip map (Data.Array.range 1 10) \n ->
+              [ [ [ "$", _1_1_equation deck hand n, "\\%", "$" ]
+                    # Data.Array.fold
+                ]
+              , flip map (Data.Array.range 1 3) \u ->
+                  [ "$", "+", _1_3_equation deck hand n v u, "\\%", "$" ]
+                    # Data.Array.fold
+              ]
+                # Data.Array.fold
+      ]
+        # Data.Array.fold
+    )
 
-printTable :: String -> Array (Array String) -> String
-printTable legend xss =
-  [ printHeader legend (Data.Maybe.maybe 0 Data.Array.length (Data.Array.head xss))
-  , printBody
-      $ Data.Array.zipWith Data.Array.cons
-          (show <$> Data.Array.range 1 10)
-      $ xss
+_1_4 :: Int -> Int -> Effect Unit
+_1_4 v u = do
+  let
+    deck :: Int
+    deck = 40
+
+    hand :: Int
+    hand = 5
+  Effect.Class.Console.log
+    $ print2DTable "n \\ m"
+    $ flip map (Data.Array.range 1 10) \n ->
+        flip map (Data.Array.range 1 10) \m ->
+          [ "$", "+", _1_4_equation deck hand n m v u, "\\%", "$" ]
+            # Data.Array.fold
+
+print1DTable :: String -> String -> Array String -> String
+print1DTable h1 h2 xs =
+  [ printHeader [ h1, h2 ]
+  , printBody (pure <$> xs)
   ]
     # Data.Array.fold
 
-printHeader :: String -> Int -> String
-printHeader legend n =
-  [ printRow ([ legend ] <> (show <$> Data.Array.range 1 n))
-  , printRow (Data.Array.replicate (n + 1) "---")
+print2DTable :: String -> Array (Array String) -> String
+print2DTable legend xss =
+  [ printHeader
+      ( [ [ legend ]
+        , ( Data.Array.range 1
+              (Data.Maybe.maybe 0 Data.Array.length (Data.Array.head xss))
+              <#> show
+          )
+        ]
+          # Data.Array.fold
+      )
+  , printBody xss
+  ]
+    # Data.Array.fold
+
+printHeader :: Array String -> String
+printHeader hs =
+  [ printRow hs
+  , printDashes (Data.Array.length hs)
   ]
     # Data.Array.fold
 
 printBody :: Array (Array String) -> String
-printBody xss = Data.Array.fold (printRow <$> xss)
+printBody xss =
+  printRows
+    $ Data.Array.zipWith Data.Array.cons
+        (show <$> Data.Array.range 1 (Data.Array.length xss))
+    $ xss
+
+printDashes :: Int -> String
+printDashes n = printRow (Data.Array.replicate n "---")
+
+printRows :: Array (Array String) -> String
+printRows xss = Data.Array.fold (printRow <$> xss)
 
 printRow :: Array String -> String
 printRow xs =
@@ -73,6 +134,20 @@ printRow xs =
   , "\n"
   ]
     # Data.Array.fold
+
+_1_1_equation :: Int -> Int -> Int -> String
+_1_1_equation deck hand n =
+  Data.Decimal.toFixed 2 <<< Combination.AST.evalExpr
+    $ Combination.AST.Multiply
+        (Combination.AST.Int 100)
+        ( Combination.AST.Divide
+            ( Combination.AST.Sum { from: 1, to: min n hand } \i ->
+                Combination.AST.Multiply
+                  (Combination.AST.Binomial n i)
+                  (Combination.AST.Binomial (deck - n) (hand - i))
+            )
+            (Combination.AST.Binomial deck hand)
+        )
 
 _1_2_equation :: Int -> Int -> Int -> Int -> String
 _1_2_equation deck hand n m =
@@ -112,6 +187,54 @@ _1_3_equation deck hand n v u =
                                 (Combination.AST.Binomial (deck - hand - n) (v - j))
                           )
                       )
+                )
+                ( Combination.AST.Multiply
+                    (Combination.AST.Binomial deck hand)
+                    (Combination.AST.Binomial (deck - hand) v)
+                )
+            )
+        )
+
+_1_4_equation :: Int -> Int -> Int -> Int -> Int -> Int -> String
+_1_4_equation deck hand n m v u =
+  Data.Decimal.toFixed 2
+    $ Combination.AST.evalExpr
+        ( Combination.AST.Multiply
+            (Combination.AST.Int 100)
+            ( Combination.AST.Divide
+                ( Combination.AST.Plus
+                    ( Combination.AST.Sum { from: 1, to: min u hand } \i ->
+                        Combination.AST.Multiply
+                          (Combination.AST.Binomial u i)
+                          ( Combination.AST.Sum { from: 1, to: min m (hand - i) } \j ->
+                              Combination.AST.Multiply
+                                (Combination.AST.Binomial m j)
+                                ( Combination.AST.Multiply
+                                    (Combination.AST.Binomial (deck - n - m - u) (hand - i - j))
+                                    ( Combination.AST.Sum { from: 1, to: min n v } \k ->
+                                        Combination.AST.Multiply
+                                          (Combination.AST.Binomial n k)
+                                          (Combination.AST.Binomial (deck - hand - n) (v - k))
+                                    )
+                                )
+                          )
+                    )
+                    ( Combination.AST.Sum { from: 1, to: min u hand } \i ->
+                        Combination.AST.Multiply
+                          (Combination.AST.Binomial u i)
+                          ( Combination.AST.Sum { from: 1, to: min n (hand - i) } \j ->
+                              Combination.AST.Multiply
+                                (Combination.AST.Binomial n j)
+                                ( Combination.AST.Multiply
+                                    (Combination.AST.Binomial (deck - n - m - u) (hand - i - j))
+                                    ( Combination.AST.Sum { from: 1, to: min m v } \k ->
+                                        Combination.AST.Multiply
+                                          (Combination.AST.Binomial m k)
+                                          (Combination.AST.Binomial (deck - hand - m) (v - k))
+                                    )
+                                )
+                          )
+                    )
                 )
                 ( Combination.AST.Multiply
                     (Combination.AST.Binomial deck hand)
